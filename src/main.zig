4e5f92a2b0318481dc46db82d68eb064785d707a
Scott Downie <scott.downie@live.com>
2022-11-09 18:03:18 +0000

    Test rendering of sprite

--- src/main.zig
+++ src/main.zig
@@ -5,19 +5,30 @@ const pd = @cImport({
 });
 
 var playdate_api: *pd.PlaydateAPI = undefined;
+var player_sprite: *pd.LCDBitmap = undefined;
 
 pub export fn eventHandler(playdate: [*c]pd.PlaydateAPI, event: pd.PDSystemEvent, _: c_ulong) callconv(.C) c_int {
     switch (event) {
         pd.kEventInit => {
             playdate_api = playdate;
-            playdate.*.system.*.setUpdateCallback.?(update, null);
+            gameInit();
+            playdate.*.system.*.setUpdateCallback.?(gameUpdate, null);
         },
         else => {},
     }
     return 0;
 }
 
-fn update(_: ?*anyopaque) callconv(.C) c_int {
+fn gameInit() void {
+    //Spawn the player sprite
+    const graphics = playdate_api.graphics.*;
+    player_sprite = graphics.loadBitmap.?("Test0.pdi", null).?;
+    //std.debug.assert(player_sprite != null);
+    //Load the enemies sprite pool
+    //Init the systems
+}
+
+fn gameUpdate(_: ?*anyopaque) callconv(.C) c_int {
     const playdate = playdate_api;
     const graphics = playdate.graphics.*;
     const sys = playdate.system.*;
@@ -26,6 +37,8 @@ fn update(_: ?*anyopaque) callconv(.C) c_int {
     graphics.clear.?(pd.kColorWhite);
     sys.drawFPS.?(0, 0);
     _ = graphics.drawText.?("hello world!", 12, pd.kASCIIEncoding, 100, 100);
+
+    graphics.drawBitmap.?(player_sprite, 200, 100, pd.kBitmapUnflipped);
 
     const shouldFire = firingSystemUpdate(sys);
     if (shouldFire) {
@@ -36,6 +49,8 @@ fn update(_: ?*anyopaque) callconv(.C) c_int {
 }
 
 var crank_angle_since_fire: f32 = 0.0;
+/// Fire everytime the crank moves through 360 degrees
+///
 fn firingSystemUpdate(sys: pd.playdate_sys) bool {
     const crank_delta = sys.getCrankChange.?();
     if (crank_delta >= 0) {
