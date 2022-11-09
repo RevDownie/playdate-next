568734c7f14351d6c013c5b2fd530cc35e02e076
Scott Downie <scott.downie@live.com>
2022-11-09 18:02:56 +0000

    Automating asset copy and pdc from zig

--- build.zig
+++ build.zig
@@ -16,10 +16,8 @@ pub fn build(b: *Builder) !void {
         "C_API",
     });
 
-    const output_path = try std.fs.path.join(b.allocator, &.{ b.install_path, "Source" });
-
+    //Create simulator DLL
     const simulator = b.addSharedLibrary("pdex", "src/main.zig", .unversioned);
-    simulator.setOutputDir(output_path);
     simulator.addIncludeDir(c_sdk_path);
     simulator.defineCMacro("TARGET_SIMULATOR", null);
     simulator.defineCMacro("TARGET_EXTENSION", null);
@@ -29,4 +27,53 @@ pub fn build(b: *Builder) !void {
     simulator.setTarget(target);
     b.default_step.dependOn(&simulator.step);
     simulator.install();
+
+    //Create empty bin folder needed to run pdc
+    const empty_bin_step = b.step("sim-bin", "Create bin file for simulator");
+    empty_bin_step.makeFn = createEmptyBin;
+    b.getInstallStep().dependOn(empty_bin_step);
+
+    //Copy assets ready for pdc
+    const copy_assets_step = b.step("assets", "Copy the assets into the build folder to be pdc'd");
+    copy_assets_step.makeFn = copyAssets;
+    b.getInstallStep().dependOn(copy_assets_step);
+
+    //pdc step to create pdx package
+    const pdc_step = b.addSystemCommand(&.{ "pdc", "-k", "zig-out/lib", "zig-out/playdate_next.pdx" });
+    pdc_step.step.dependOn(copy_assets_step);
+    b.getInstallStep().dependOn(&pdc_step.step);
+}
+
+fn createEmptyBin(_: *std.build.Step) !void {
+    std.debug.print("Creating simulator bin file\n", .{});
+    if (std.fs.cwd().createFile("zig-out/lib/pdex.bin", .{})) |file| {
+        file.close();
+    } else |err| switch (err) {
+        error.PathAlreadyExists => {},
+        else => return err,
+    }
+}
+
+fn copyAssets(_: *std.build.Step) !void {
+    std.debug.print("Copying assets...\n", .{});
+    var output_dir = try std.fs.cwd().openDir("zig-out/lib", .{});
+    defer output_dir.close();
+
+    //pdxinfo
+    try std.fs.cwd().copyFile("pdxinfo", output_dir, "pdxinfo", .{});
+
+    //Images
+    var image_dir = try std.fs.cwd().openDir(
+        "images",
+        .{ .iterate = true },
+    );
+    defer image_dir.close();
+
+    var iter = image_dir.iterate();
+    while (try iter.next()) |entry| {
+        if (entry.kind == .File) {
+            std.debug.print("Copying image: {s}\n", .{entry.name});
+            try image_dir.copyFile(entry.name, output_dir, entry.name, .{});
+        }
+    }
 }
